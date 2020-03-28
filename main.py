3bdd25a56d3f8c10e751259a5f23f13573f7517a
Hollis Ma <hollisma@pop-os.localdomain>
2020-03-28 12:44:47 -0400

    basic input page for features and display

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+@app.route('/analyze', methods=['POST', 'GET'])
+def analyze():
+  if request.method == 'POST':
+    features = []
+    """ 
+      get features from request.form[]
+      analyze stuff 
+    """
+    return render_template('output.html', features=features)
+    # return redirect(url_for('output.html', features=features))
+  elif request.method == 'GET':
+    features = []
+    """ 
+      get features from request.args.get()
+    """
+    return render_template('output.html', features=features)
+    # return redirect(url_for('output.html', features=features))
+
+
+# Below are just test routes
+
 @app.route('/hello/<name>')
 def hello(name):
   # return 'Hello %s!' % name
   return render_template('hello.html', name=name)
 
 @app.route('/')
-@app.route('/login')
 def login():
   return render_template('login.html')
 
