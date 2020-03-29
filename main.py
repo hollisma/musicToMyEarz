2a45acf99d3e491b1ccf44db0dd4e6fb9044c804
Hollis Ma <hollisma@pop-os.localdomain>
2020-03-29 12:39:48 -0400

    updated frontend and connection to main.py

--- main.py
+++ main.py
@@ -13,8 +13,22 @@ def analyze():
 
     """ get features from request.form[]
         analyze stuff """
-    features['name'] = request.form['name'] + ', ' if request.form['name'] != None else ''
-        
+    features['name'] = request.form['name'] + ', ' if request.form['name'].strip() != "" else ''
+    features['genre'] = request.form['genre']
+    features['acousticness'] = int(request.form['acousticness'])
+    features['danceability'] = request.form['danceability'] 
+    features['duration'] = float(request.form['duration']) * 1000 if request.form['duration'].strip() != "" else 0
+    features['energy'] = int(request.form['energy']) 
+    features['instrumentalness'] = int(request.form['instrumentalness']) 
+    features['key'] = request.form['key']
+    features['liveness'] = int(request.form['liveness']) 
+    features['loudness'] = int(request.form['loudness']) 
+    features['mode'] = request.form['mode'] 
+    features['speechiness'] = int(request.form['speechiness']) 
+    features['tempo'] = int(request.form['tempo']) 
+    features['time_signature'] = int(request.form['time_signature'])
+    features['valence'] = int(request.form['valence']) 
+
     return render_template('output.html', features=features)
     # return redirect(url_for('output.html', features=features))
   elif request.method == 'GET':
