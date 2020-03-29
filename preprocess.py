17b7a56e3716faef54d4b4a316d34d500b104535
mazeiomli <li.cz.eric@gmail.com>
2020-03-28 21:16:04 -0400

    organize preprocess.py, get_stratified_df() should work

--- preprocess.py
+++ preprocess.py
@@ -1,62 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-print("Successfully Loaded")
+# print("Successfully Loaded modules for preprocess.py")
 
 
-# In[ ]:
 
 
-# Read CSV
-df = pd.read_csv("data/SpotifyFeatures.csv")
-# df_top = df.head()
-
-# df_top
-
-df
 
 
-# In[ ]:
-
-
-df.dtypes
-
-
-# In[ ]:
-
-
-df['genre'].unique()
-
-
-# In[ ]:
-
-
-"""
-Consolidate children's music. Two values for same category.
-"""
-
-print(np.sum(df['genre'] == "Children's Music"))
-print(np.sum(df['genre'] == 'Children’s Music'))
-
-new_gen = df['genre'].replace('Children’s Music', "Children's Music")
-
-
-print(np.sum(new_gen == "Children's Music"))
-print(np.sum(new_gen == 'Children’s Music'))
-
-df['genre'] = new_gen.astype('category')
-
-
-# In[ ]:
-
-
-df['genre']
 
 
 # ### Potential Issues:
@@ -64,97 +18,66 @@ df['genre']
 # 
 # If a song is in a certain key, then there are 0's in all other columns of keys. In other words, the values of the columns of keys are dependent on the other such columns.
 
-# In[ ]:
+
+
+"""
+Consolidate children's music. Two values for same category.
+"""
+def combine_child_music(df):
+    new_gen = df['genre'].replace('Children’s Music', "Children's Music")
+    df['genre'] = new_gen.astype('category')
+    return df
 
 
 """
 Change keys into 0-1 columns
 """
-
-df['key'].unique()
-for k in df['key'].unique():
-    # k == df['key'] gives boolean series
-    df[k] = (k == df['key']).astype('int64')
-    
-df = df.drop(labels = 'key', axis = 'columns')
-
-
-# In[ ]:
-
-
-df.dtypes
-
-
-# In[ ]:
-
-
-df['time_signature'].unique()
-
-
-# In[ ]:
+def keys_to_binary_cols(df):
+    for k in df['key'].unique():
+        # k == df['key'] gives boolean series
+        df[k] = (k == df['key']).astype('int64')
+        
+    return df.drop(labels = 'key', axis = 'columns')
 
 
 """
 Change time signature into 0-1 columns
 """
-
-for ts in df['time_signature'].unique():
-    # k == df['key'] gives boolean series
-    df[ts] = (ts == df['time_signature']).astype('int64')
-    
-df = df.drop(labels = 'time_signature', axis = 'columns')
-
-
-# In[ ]:
-
-
-df.dtypes
-
-
-# In[ ]:
+def time_sig_to_binary_cols(df):
+    for ts in df['time_signature'].unique():
+        # k == df['key'] gives boolean series
+        df[ts] = (ts == df['time_signature']).astype('int64')
+        
+    return df.drop(labels = 'time_signature', axis = 'columns')
 
 
 """
 Turn mode (major/minor) into a numerical column (0 for minor, 1 for major)
 """
-
-df['mode'] = (df['mode'] == 'Major').astype('int64') 
-
-
-# In[ ]:
+def mode_to_binary_col(df):
+    df['mode'] = (df['mode'] == 'Major').astype('int64')
+    return df
 
 
-df.dtypes
 
 
-# In[ ]:
+"""
+The function preprocesses the dataframe. Then returns a dictionary
+of key: genre and value: dataframe of songs of only that genre
 
-
-# histogram of popularity
-pop = df['popularity'].to_numpy()
-
-n, bins, patches = plt.hist(pop, bins = 50, density=False, facecolor='g')
-
-plt.xlabel("popularity")
-plt.ylabel("frequency")
-plt.show()
-
-
-# In[ ]:
-
-
-# Separate larger dataframe into dataframes of all of one category
-# dictionary that maps genre to the dataframe that only has songs of that genre
-df_by_genre = dict()
-for g in df['genre'].unique():
-    df_by_genre[g] = df[df['genre'] == g]
-
-
-# In[ ]:
-
-
-# Find number of songs for each genre
-
-for g,df_g in df_by_genre.items():
-    print(g + ": " + str(df_g.shape[0]))
+"""
+def get_stratified_df():
+    # Read CSV
+    df_orig = pd.read_csv("data/SpotifyFeatures.csv")
+    # list of functions that modify df
+    funcs = [combine_child_music, keys_to_binary_cols, time_sig_to_binary_cols, mode_to_binary_col]
+    df = df_orig
+    for f in funcs:
+        df = f(df)
+    # Separate larger dataframe into dataframes of all of one category
+    # dictionary that maps genre to the dataframe that only has songs of that genre
+    df_by_genre = dict()
+    for g in df['genre'].unique():
+        df_by_genre[g] = df[df['genre'] == g]
+    return df_by_genre
 
